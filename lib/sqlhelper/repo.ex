5fb01520d34e40a57d4646ba51920f05d85366e7
TomBers <tjcberman@gmail.com>
2023-09-19 14:35:24 +0100

    add query history and saved rows

--- lib/sqlhelper/repo.ex
+++ lib/sqlhelper/repo.ex
@@ -2,4 +2,7 @@ defmodule Sqlhelper.Repo do
   use Ecto.Repo,
     otp_app: :sqlhelper,
     adapter: Ecto.Adapters.Postgres
+
+  # https://hexdocs.pm/ecto/Ecto.Repo.html#module-read-only-repositories
+  # , read_only: true
 end
--- lib/sqlhelper_web/live/sql_live.ex
+++ lib/sqlhelper_web/live/sql_live.ex
@@ -13,24 +13,56 @@ defmodule SqlhelperWeb.SqlLive do
        error: nil,
        challenge: challenge,
        guess: "",
-       answer: ""
+       answer: "",
+       query_history: [],
+       saved_results: []
      )}
   end
 
   def handle_event("execute", %{"query" => query}, socket) do
-    IO.inspect(Sqlhelper.Repo.query(query))
+    query_history = [query | socket.assigns.query_history]
 
     case Sqlhelper.Repo.query(query) do
       {:ok, %{columns: cols, rows: rows}} ->
-        {:noreply, assign(socket, query: query, columns: cols, result: rows, error: nil)}
+        {:noreply,
+         assign(socket,
+           query: query,
+           columns: cols,
+           result: rows,
+           error: nil,
+           query_history: query_history
+         )}
 
       {:error, %{postgres: %{message: error}}} ->
-        {:noreply, assign(socket, error: error)}
+        {:noreply, assign(socket, error: error, query_history: query_history)}
     end
+  end
+
+  def handle_event("save_result", %{"row" => row}, socket) do
+    # Add the row to the saved results
+    saved_results = [row | socket.assigns.saved_results]
+
+    {:noreply, assign(socket, saved_results: saved_results)}
   end
 
   def handle_event("guess", %{"guess" => guess}, socket) do
     answer = if guess == socket.assigns.challenge.answer, do: "Correct!", else: "Incorrect."
     {:noreply, assign(socket, answer: answer)}
   end
+
+  def filter_row_value(row) do
+    row
+    |> Enum.map(fn v ->
+      if is_boolean(v) do
+        map_bool_to_string(v)
+      else
+        v
+      end
+    end)
+    |> Enum.filter(fn v -> is_integer(v) or is_bitstring(v) end)
+    |> IO.inspect()
+  end
+
+  defp map_bool_to_string(true), do: "true"
+  defp map_bool_to_string(false), do: "false"
 end
