f0fc149564ee7ea065a8cf188ea6607e817fdfe1
TomBers <tjcberman@gmail.com>
2023-09-29 23:57:41 +0100

    styled some of the comps

--- lib/sqlhelper_web/live/table.ex
+++ lib/sqlhelper_web/live/table.ex
@@ -30,13 +30,15 @@ defmodule SqlhelperWeb.Table do
 
   attr :columns, :list, default: []
   attr :rows, :list, default: []
+  attr :title, :string, default: "Table"
   attr :allow_save, :boolean, default: false
   attr :allow_delete, :boolean, default: false
 
   def sql_table(assigns) do
     ~H"""
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 overflow-x-auto">
-      <table class="min-w-full divide-y divide-gray-200">
+      <h1 class="text-lg"><%= @title %></h1>
+      <table class="min-w-full divide-y divide-gray-200 border-solid">
         <thead class="bg-gray-50">
           <tr>
             <%= for col <- @columns do %>
@@ -67,7 +69,7 @@ defmodule SqlhelperWeb.Table do
                     phx-click="save_result"
                     phx-value-row={format_query_result(row)}
                     phx-value-col={format_columns(@columns)}
-                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    class="btn"
                   >
                     Save
                   </button>
@@ -79,7 +81,7 @@ defmodule SqlhelperWeb.Table do
                     phx-click="delete_result"
                     phx-value-row={format_query_result(row)}
                     phx-value-col={format_columns(@columns)}
-                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    class="btn"
                   >
                     Delete
                   </button>
