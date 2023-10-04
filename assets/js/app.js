bf0c8c5d242223b9abfa135e31b2f0fe974710f0
TomBers <tjcberman@gmail.com>
2023-10-04 19:18:59 +0100

    add hooks for keyboard shortcut

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,21 @@ import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
+
+let Hooks = {}
+
+Hooks.CtrlEnterSubmit = {
+    mounted() {
+        this.el.addEventListener("keydown", (e) => {
+            if (e.ctrlKey && e.key === 'Enter') {
+                this.el.form.dispatchEvent(
+                    new Event('submit', { bubbles: true, cancelable: true }));
+            }
+        })
+    }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
